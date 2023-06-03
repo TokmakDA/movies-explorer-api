f5a94ff689d1ce7229151bbec777ad00746aa06c
Dmitry <113120694+TokmakDA@users.noreply.github.com>
2023-06-03 13:22:14 +0300

    fix rout

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(cors(corsOptions));
 app.use('/api', routes);
-app.use('*', (req, res, next) => {
+app.use((req, res, next) => {
   next(new NotFoundError(NOT_FOUND_DEFAULT_ERROR_RU));
 });
 app.use(errorLogger); // подключаем логгер ошибок
