309bd73bb2381bf33241bef39f34a662b064fa8b
Dmitry <113120694+TokmakDA@users.noreply.github.com>
2023-06-04 08:10:22 +0300

    Fix routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const { NOT_FOUND_DEFAULT_ERROR_RU } = require('../data');
 const { NotFoundError } = require('../errors');
 const auth = require('../middlewares/auth');
+const notFound = require('../middlewares/notFound');
 const movieRouter = require('./movies');
 const signRouter = require('./signing');
 const userRouter = require('./users');
@@ -11,6 +12,4 @@ module.exports = require('express')
   .use(auth)
   .use('/movies', movieRouter)
   .use('/users', userRouter)
-  .use('*', (req, res, next) => {
-    next(new NotFoundError(NOT_FOUND_DEFAULT_ERROR_RU));
-  }); // 404 ошибка неверного пути
+  .use(notFound); // 404 ошибка неверного пути
