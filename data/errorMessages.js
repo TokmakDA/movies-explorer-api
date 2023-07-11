38c30d2943b47be734e6b9ad060f528af110f76f
Dmitry <113120694+TokmakDA@users.noreply.github.com>
2023-07-11 20:40:23 +0300

    Correct Error message

--- data/errorMessages.js
+++ data/errorMessages.js
@@ -1,13 +1,16 @@
-const DEFAULT_MESSAGE_ERROR_RU = 'Что-то пошло не так';
-const CONFLICT_MOVIE_CARD_ERROR_RU = 'Карточка с фильмом уже добавлена';
-const CONFLICT_USER_CARD_ERROR_RU = 'Пользователь с введенным Email уже существует';
-const FORBIDDEN_MOVIE_CARD_ERROR_RU = 'Вы не являетесь владельцем данной карточки с фильмом';
-const NOT_FOUND_USER_CARD_ERROR_RU = 'Пользователь не найден';
-const NOT_FOUND_MOVIE_CARD_ERROR_RU = 'Карточка с фильмом не найдена';
-const NOT_FOUND_DEFAULT_ERROR_RU = 'Страница не найдена';
-const LIMITTER_ERROR_RU = 'Слишком много запросов с одного IP. Попробуйте позже';
-const AUTHORIZATION_REQUIRED_ERROR_RU = 'Необходима авторизация';
-const AUTHORIZATION_WRONG_ERROR_RU = 'Неправильные почта или пароль';
+const DEFAULT_MESSAGE_ERROR_RU = 'На сервере произошла ошибка.';
+const CONFLICT_MOVIE_CARD_ERROR_RU = 'Карточка с фильмом уже добавлена.';
+const CONFLICT_USER_CARD_ERROR_RU =
+  'Пользователь с введенным Email уже существует.';
+const FORBIDDEN_MOVIE_CARD_ERROR_RU =
+  'Вы не являетесь владельцем данной карточки с фильмом.';
+const NOT_FOUND_USER_CARD_ERROR_RU = 'Пользователь не найден.';
+const NOT_FOUND_MOVIE_CARD_ERROR_RU = 'Карточка с фильмом не найдена.';
+const NOT_FOUND_DEFAULT_ERROR_RU = 'Страница по указанному маршруту не найдена.';
+const LIMITTER_ERROR_RU =
+  'Слишком много запросов с одного IP. Попробуйте позже.';
+const AUTHORIZATION_REQUIRED_ERROR_RU = 'Необходима авторизация.';
+const AUTHORIZATION_WRONG_ERROR_RU = 'Вы ввели неправильный логин или пароль.';
 
 module.exports = {
   DEFAULT_MESSAGE_ERROR_RU,
